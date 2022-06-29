6a7ff610b100e0a31323afdeb45acb85962f298e
jvrmalv <jvrmalv@gmail.com>
2022-06-29 15:40:29 -0300

    Revert "feat: add create function and user changeset"
    This reverts commit 82a67a97df420b66e6b9753158e6439e247e47d6.

--- lib/anime_list/accounts.ex
+++ lib/anime_list/accounts.ex
@@ -2,18 +2,8 @@ defmodule AnimeList.Accounts do
 
   alias Animelist.User
 
-  @type create_args %{
-    required(:name) => String.t(),
-    required(:email) => String.t(),
-    required(:password) => String.t(),
-    optional(:anime) => [String.t()]
-  }
+  def create(%{id: id, username: username, password: password}) do
 
-  @spec create(create_args) :: {:ok, type} | {:error, type}
-  def create(attrs) do
-   %User
-   |> User.changeset(attrs)
-   |> Repo.insert()
   end
 
 end
