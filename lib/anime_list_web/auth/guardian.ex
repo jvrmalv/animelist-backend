7aae675141ac24f181389091664aa123f6f34154
jvrmalv <jvrmalv@gmail.com>
2022-07-16 18:22:17 -0300

    feat: added sign authentication

--- lib/anime_list_web/auth/guardian.ex
+++ lib/anime_list_web/auth/guardian.ex
@@ -8,9 +8,30 @@ defmodule AnimeListWeb.Auth.Guardian do
     {:ok, sub}
   end
 
+
   def resource_from_claims(claims) do
     id = claims["sub"]
     resource = Accounts.get_user!(id)
     {:ok, resource}
+  end
+
+  def authenticate(email, password) do
+    with {:ok, user} <- Accounts.get_by_email(email) do
+      case validate_password(user, password) do
+        {:ok, _user}->
+          create_token(user)
+        {:error, error } ->
+          {:error, :unauthorized}
+      end
+    end
+  end
+
+  defp validate_password(user, password) do
+    Argon2.check_pass(user, password)
+  end
+
+  defp create_token(user) do
+    {:ok, token, _claims} = encode_and_sign(user)
+    {:ok, user, token}
   end
 end
